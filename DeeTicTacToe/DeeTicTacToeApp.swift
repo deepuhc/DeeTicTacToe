3e816aaa00851fe58fa9bf84585ad0bda88c58ea
Deepak Chandrashekhar <hcdeepu@gmail.com>
2025-02-24 22:05:06 -0500

    Initial Commit

---
+++ DeeTicTacToe/DeeTicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeeTicTacToeApp.swift
+//  DeeTicTacToe
+//
+//  Created by Deepak Deepak on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct DeeTicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
